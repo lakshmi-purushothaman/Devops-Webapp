36276a2f791b0dfebd5af711e7429c687515d31a
Lakshmi Purushotham <lakshmi@Lakshmis-MacBook-Air.local>
2023-03-30 07:55:00 +0100

    Added Tests

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 ## Simple Flask Web App
-
-This is the source code for the tutorial on https://www.learnaws.org/.
---
+++ test/test_hello_world.py
@@ -0,0 +1,21 @@
+import pytest, logging
+
+from web import app, index
+
+#Logging configurations
+logging.basicConfig(level=logging.DEBUG)
+
+@pytest.fixture
+def app_inst():
+    #Pytest fixture that returns an instance of application.
+    app.debug = True
+    return app
+
+def test_index():
+    index_str = index()
+    assert index_str == "Hello, World!"
+
+def test_index_200(app_inst):
+    client = app_inst.test_client()
+    response = client.post("/")
+    assert response.status_code == 200
